46d30d5e55cf193a8970413124292b968d1a28e8
daisuke koguchi <ds05080906@gmail.com>
2022-01-31 18:39:09 +0900

    [add] application.html.erbにゲストログインのハイパーリンク作成/issue #5

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'users#mypage'
   get 'users/mypage' => 'users#mypage'
   devise_for :users
+  devise_scope :user do 
+    post 'users/guest_sign_in' => 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
