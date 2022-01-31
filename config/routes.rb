d5dde88f9a384d2620c3886b5843dc81ba3a59a4
daisuke koguchi <ds05080906@gmail.com>
2022-01-31 21:59:32 +0900

    [add]ゲストログインユーザーがアカウント削除できないよう設定/issue #5

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'users#mypage'
   get 'users/mypage' => 'users#mypage'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   devise_scope :user do 
     post 'users/guest_sign_in' => 'users/sessions#guest_sign_in'
   end
