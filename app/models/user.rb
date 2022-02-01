8c8044c9bf494da74ec4c39c1412f99214d88235
daisuke koguchi <ds05080906@gmail.com>
2022-02-01 14:48:12 +0900

    [add]application.html.erbに論理削除のリンクを作成。リンクをクリックすると、ユーザーの論理削除可能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable 
   
+  def soft_delete 
+    update_attribute(:deleted_at, Time.current)
+  end
+
+  def active_for_authentication?
+    super && !deleted_at
+  end
+
+  def inactive_message 
+    !deleted_at ? super : :deleted_account 
+  end
+  
   def self.guest 
     find_or_create_by!(email: 'guest@example.com') do |user| 
       user.password = SecureRandom.urlsafe_base64
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_011809) do
+ActiveRecord::Schema.define(version: 2022_02_01_051140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_02_01_011809) do
     t.string "last_name", null: false
     t.string "avatar_image"
     t.text "introduction", null: false
+    t.datetime "deleted_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
