44eeeb4ecfbe30fd18f7d4d3d74406110a2daffb
daisuke koguchi <ds05080906@gmail.com>
2022-02-02 16:41:45 +0900

    [add]ユーザーコントローラーにtopアクションを追加。top.html.erb,mypage.html.erbを修正/issue #11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i{mypage}
 
   def top
-    # if user_signed_in?
-    #   redirect_to mypage_path
-    # end
   end
 
   def mypage
+    @user = User.find(params[:id])
   end
   
-  def set_user 
-     @user = User.find(params[:id])
-  end
 end
