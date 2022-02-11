d9c18130833495758cae5ca6d876f8de4a871e9d
daisuke koguchi <ds05080906@gmail.com>
2022-02-11 12:12:45 +0900

    [add]ユーザー登録テスト実施。新規登録ボタンを押すと、確認画面に遷移。確認画面から完了画面に遷移することを確認/feature #7

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :user do 
     first_name {"テスト"}
     last_name {"ユーザー"}
-    avater_image{Rack::Test::UploadedFile.new(File.join(Rails.root,'spec/fixtures/test.png'))}
+    avatar_image{Rack::Test::UploadedFile.new(File.join(Rails.root,'spec/fixtures/test.png'))}
     introduction{"テストを開始します。よろしくお願いします。"}
-    email { |n| "test#{n}example.rb"}
+    email { |n| "test@example.rb"}
     password {"password"}
     password_confirmation {"password"}
   end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 RSpec.describe 'dvevise機能', type: :system do
   describe '新規会員登録機能' do
-    context '新規会員登録画面でユーザが入力した場合' do 
-      it '確認画面にユーザーの登録情報が表示される' do
-        visit new_user_registration_path
+    let!(:user){FactoryBot.build(:user)}
+    before do 
+      visit new_user_registration_path
         fill_in 'user[first_name]', with: 'テスト'
         fill_in 'user[last_name]', with: 'ユーザー'
         attach_file 'user[avatar_image]', "#{Rails.root}/spec/fixtures/test.png"
@@ -11,6 +11,9 @@ RSpec.describe 'dvevise機能', type: :system do
         fill_in 'user[email]',with: 'test@example.rb'
         fill_in 'user[password]',with: 'password'
         fill_in 'user[password_confirmation]',with:'password'
+    end
+    context '新規会員登録画面でユーザが入力した場合' do 
+      it '確認画面にユーザーの登録情報が表示される' do
         click_on 'アカウント登録'
       expect(page).to have_content('Confirm') #レイアウト変更時修正必要
       end
@@ -18,6 +21,9 @@ RSpec.describe 'dvevise機能', type: :system do
     context '新規会員会員登録画面で登録画面を押した場合'do 
 
     it '登録完了画面に移動する' do
+      click_on 'アカウント登録'
+      click_on '登録する'
+      expect(page).to have_content('登録が完了しました') #レイアウト変更時修正必要
       end
     end
 
