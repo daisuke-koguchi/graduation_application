6ae6076b22c5410272fc625ace89284130415e2f
daisuke koguchi <ds05080906@gmail.com>
2022-02-12 13:22:37 +0900

    [add]User Controllerで他のユーザーのマイページへの移動を制限.ログインユーザーのみアクセス出来るよう制限/feature #71

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,25 @@
 class UsersController < ApplicationController
-
+  before_action :authenticate_user!, only: %i{ mypage index show }
+  before_action :set_user, only: %i{ mypage show }
   def top
   end
 
   def mypage
-    @user = User.find(params[:id])
+    if @user.id !=  current_user.id
+      redirect_to users_mypage_path(id: current_user.id), alert: "アクセス権限がありません" 
+    end
   end
   
   def index
     @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true).page(params[:page])
+    @users = @q.result(distinct: true).page(params[:page]).per(5)
   end
 
   def show 
-    @user = User.find(params[:id])
   end
 
+  private 
+  def set_user 
+    @user = User.find(params[:id])
+  end
 end
