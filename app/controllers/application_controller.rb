11fb8134e982a055abbbffa913da24c51d1d059a
daisuke koguchi <ds05080906@gmail.com>
2022-02-15 00:09:45 +0900

    [add]application.controllerのdevise_parameter_sanitizerにnick_name追加。新規会員ページにフォーム作成/feature#15

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar_image, :introduction,:avatar_image_cache])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar_image, :introduction,:avatar_image_cache])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar_image, :introduction,:avatar_image_cache, :nick_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar_image, :introduction,:avatar_image_cache, :nick_name])
   end
 end
