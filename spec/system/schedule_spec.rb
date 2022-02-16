36cc885b593e380b656e679f3dc6be845438357a
daisuke koguchi <ds05080906@gmail.com>
2022-02-16 15:13:30 +0900

    [add] RSpecテスト実施する前にレイアウトを修正する/feature #51

---
+++ spec/system/schedule_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+RSpec.describe 'スケジュール機能', type: :system do
+  describe '運動スケジュール表示機能' do
+    context '当日の運動を登録すると' do
+      it '運動内容が表示される' do
+
+      end
+    end
+    context ''
+
+  end
+end
